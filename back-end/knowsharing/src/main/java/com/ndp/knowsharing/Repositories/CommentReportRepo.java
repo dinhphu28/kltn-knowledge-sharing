aca06514b2e912b08180ed844317f4e22d6a42e7
Anthony <18110175@student.hcmute.edu.vn>
2022-10-14 15:20:54 +0700

    Repo - Add some query method

--- back-end/knowsharing/src/main/java/com/ndp/knowsharing/Repositories/CommentReportRepo.java
+++ back-end/knowsharing/src/main/java/com/ndp/knowsharing/Repositories/CommentReportRepo.java
@@ -1,9 +1,21 @@
 package com.ndp.knowsharing.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ndp.knowsharing.Entities.CommentReport;
 
+@Repository
 public interface CommentReportRepo extends JpaRepository<CommentReport, String> {
-    
+    List<CommentReport> findByCommentId(String commentId);
+
+    List<CommentReport> findByCommentIdOrderByDateCreatedDesc(String commentId);
+
+    List<CommentReport> findByOrderByDateCreatedDesc();
+
+    Long deleteByCommentId(String commentId);
+
+    List<CommentReport> findByIsSolved(Integer isSolved);
 }
--- back-end/knowsharing/src/main/java/com/ndp/knowsharing/Services/ArticleService.java
+++ back-end/knowsharing/src/main/java/com/ndp/knowsharing/Services/ArticleService.java
@@ -1,8 +1,14 @@
 package com.ndp.knowsharing.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.ndp.knowsharing.Entities.Article;
 import com.ndp.knowsharing.Repositories.ArticleRepo;
 
 @Service
@@ -10,5 +16,15 @@ public class ArticleService {
     @Autowired
     private ArticleRepo repo;
 
-    
+    public List<Article> retrieveAll() {
+        return repo.findAll();
+    }
+
+    public List<Article> retrieveOneCommonPage(Integer pageNumber) {
+        Page<Article> page = repo.findAll(PageRequest.of(pageNumber, 10, Sort.by("dateCreated").descending()));
+
+        List<Article> articles = page.getContent();
+
+        return articles;
+    }
 }
