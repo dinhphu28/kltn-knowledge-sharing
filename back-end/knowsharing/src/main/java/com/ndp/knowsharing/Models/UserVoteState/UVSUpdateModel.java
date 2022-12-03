8ba7e3923ce9b76ab6a8d2ae67e1127ddd1b5adc
Anthony <18110175@student.hcmute.edu.vn>
2022-12-03 17:19:41 +0700

    Article Evaluation

---
+++ back-end/knowsharing/src/main/java/com/ndp/knowsharing/Models/UserVoteState/UVSUpdateModel.java
@@ -0,0 +1,14 @@
+package com.ndp.knowsharing.Models.UserVoteState;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UVSUpdateModel {
+    private Integer voteState;
+}
