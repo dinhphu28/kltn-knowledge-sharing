d9d2bc31a131380d2c390647c9887fd5b7055491
Anthony <18110175@student.hcmute.edu.vn>
2022-12-05 11:04:32 +0700

    Article Report

--- back-end/knowsharing/src/main/java/com/ndp/knowsharing/Services/ArticleReportService.java
+++ back-end/knowsharing/src/main/java/com/ndp/knowsharing/Services/ArticleReportService.java
@@ -1,8 +1,11 @@
 package com.ndp.knowsharing.Services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ndp.knowsharing.Entities.ArticleReport;
 import com.ndp.knowsharing.Repositories.ArticleReportRepo;
 
 @Service
@@ -10,5 +13,89 @@ public class ArticleReportService {
     @Autowired
     private ArticleReportRepo repo;
 
-    
+    public List<ArticleReport> retrieveAll() {
+        // return repo.findAll();
+        return repo.findByOrderByDateCreatedDesc();
+    }
+
+    public List<ArticleReport> retrieveAllByArticleId(String articleId) {
+        // return repo.findByArticleId(articleId);
+        return repo.findByArticleIdOrderByDateCreatedDesc(articleId);
+    }
+
+    public List<ArticleReport> retrieveBySolved(Integer solved) {
+        return repo.findByIsSolved(solved);
+    }
+
+    public ArticleReport retrieveById(String id) {
+        ArticleReport tmp = null;
+
+        try {
+            tmp = repo.findById(id).get();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return tmp;
+    }
+
+    public ArticleReport createOne(ArticleReport articleReport) {
+        ArticleReport tmp = null;
+
+        articleReport.setId(null);
+
+        try {
+            tmp = repo.save(articleReport);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        return tmp;
+    }
+
+    public ArticleReport updateOne(ArticleReport articleReport) {
+        ArticleReport tmpArticleReport = null;
+
+        try {
+            repo.findById(articleReport.getId()).get();
+
+            tmpArticleReport = repo.save(articleReport);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        return tmpArticleReport;
+    }
+
+    public Boolean deleteOne(String id) {
+        Boolean kk = false;
+
+        try {
+            repo.findById(id).get();
+            
+            repo.deleteById(id);
+
+            kk = true;
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        return kk;
+    }
+
+    public Boolean deleteAllByArticleId(String articleId) {
+        Boolean kk = false;
+
+        try {
+            Long ii = repo.deleteByArticleId(articleId);
+
+            if(ii > 0) {
+                kk = true;
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        return kk;
+    }
 }
